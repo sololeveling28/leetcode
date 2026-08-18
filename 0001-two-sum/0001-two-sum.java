a7f3f21d026dce92b76908bddec0ee94925258a3
sololeveling28 <boreddynavaneeswarr@gmail.com>
2026-08-18 08:45:59 +0530

    Time: 10 ms (46.01%), Space: 46.9 MB (85.47%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,19 +1,25 @@
 class Solution {
-    public int[] twoSum(int[] nums, int target) {
+    public int[] twoSum(int[] arr, int tar) {
         
-
-
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                
-                if (nums[i] + nums[j] == target) {
-                    return new int[] {i, j};
-                }
-            }
+        int n = arr.length;
+        int arr2[][]  = new int[n][2];
+        for(int i=0;i<n;i++){
+            arr2[i][0] = arr[i];
+            arr2[i][1] = i;
         }
+        Arrays.sort(arr2,(a,b)->Integer.compare(a[0],b[0]));
+        int ans[] = {-1,-1};
+        int i = 0, j = n-1;
 
-         
-        
-        return new int[] {}; 
+        while(i<j){
+            if(arr2[i][0] + arr2[j][0] == tar){
+                ans[0] = arr2[i][1];
+                ans[1] = arr2[j][1];
+                return ans;
+            }
+            else if(arr2[i][0] + arr2[j][0] > tar) j--;
+            else i++;
+        }
+        return ans;
     }
 }
